63cef12ab96d377954bce6e97b4237e3736709a5
Your Name <nmurthy@icm-consulting.com.au>
2020-08-16 20:11:32 +1000

    adding a generic exception handler to the web controller

--- ny-cab-api/ny-cab-web-api/src/main/java/com/nycab/web/api/controllers/NyCabWebController.java
+++ ny-cab-api/ny-cab-web-api/src/main/java/com/nycab/web/api/controllers/NyCabWebController.java
@@ -3,13 +3,10 @@ package com.nycab.web.api.controllers;
 import com.nycab.commons.constants.ApplicationConstants;
 import com.nycab.web.api.facade.NyCabWebFacadeIntf;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -21,8 +18,13 @@ public class NyCabWebController {
     @GetMapping(value = ApplicationConstants.WEB_GET_BY_MEDALLIONS_AND_DATE)
     public ResponseEntity getByMedallion(@PathVariable("medallions") List<String> medallions,
                                          @PathVariable("date") String date) {
-
         return ResponseEntity.ok(nyCabWebFacadeIntf.getByMedallionsAndDate(medallions, date));
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+    public @ResponseBody ResponseEntity handleException(Exception ex) {
+        return new ResponseEntity("Internal server error occurred", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
